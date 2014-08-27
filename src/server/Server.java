eff4048bc26c413e4b7a3d00abcc909cb8c47bb0
tundo91 <enrico.rotundo@gmail.com>
2014-08-27 12:37:46 +0200

    foo22

--- src/server/Server.java
+++ src/server/Server.java
@@ -8,13 +8,34 @@ import java.rmi.RemoteException;
 import java.util.Vector;
 
 import resource.ResourceInterface;
-import client.Client;
 import client.ClientInterface;
 
 public final class Server implements ServerInterface {
+	public static void main(final String[] args) throws Exception {
+		if (args.length == 0) {
+			System.out.println("invalid argument!");
+		} else {
+			// Schedule a job for the event dispatch thread:
+			// creating and showing this application's GUI.
+			javax.swing.SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						final Server server = new Server(args[0]);
+						final String rmiObjName = server.getServerUrl();
+						Naming.rebind(rmiObjName,server);
+					} catch (final Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+
+		}
+	}
 	private ServerFrame myFrame = null;
 	private static final String HOST = "localhost";
 	private Vector<ClientInterface> myConnectedClients;
+
 	private final String serverNameString;
 
 	public Server(final String paramServerName) {
@@ -36,56 +57,38 @@ public final class Server implements ServerInterface {
 
 	}
 
-//	@Override
+	//	@Override
+	@Override
 	public void clientConnect(final ClientInterface paramClient)
 			throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
 
-//	@Override
+	//	@Override
+	@Override
 	public void clientDisconnect(final ClientInterface paramClient)
 			throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
 
-//	@Override
+	//	@Override
+	@Override
 	public Vector<ClientInterface> getClients() throws RemoteException {
 		return myConnectedClients;
 	}
 
-//	@Override
+	//	@Override
+	@Override
 	public Vector<ClientInterface> getRequest(
 			final ResourceInterface paramResource) throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-//	@Override
+	@Override
 	public String getServerUrl() throws RemoteException {
 		return "rmi://" + HOST + "/Server/" + serverNameString;
-	}
-	
-	 public static void main(final String[] args) throws Exception {
-		if (args.length == 0) {
-			System.out.println("invalid argument!");
-		} else {
-			// Schedule a job for the event dispatch thread:
-			// creating and showing this application's GUI.
-			javax.swing.SwingUtilities.invokeLater(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						Server server = new Server(args[0]);
-						String rmiObjName = server.getServerUrl();
-						Naming.rebind(rmiObjName,server);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			});			
-
-		}
 	}
 }
