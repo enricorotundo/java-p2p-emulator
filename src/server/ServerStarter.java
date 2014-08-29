c2d9287ddfbaeb77e48ad67c47e4f6d8a0a97ad2
tundo91 <enrico.rotundo@gmail.com>
2014-08-29 18:16:13 +0200

    add namesake server check

--- src/server/ServerStarter.java
+++ src/server/ServerStarter.java
@@ -4,18 +4,32 @@ import java.rmi.Naming;
 
 
 public class ServerStarter {
-
 	public static void main(final String[] args) throws Exception {
 		if (args.length == 0) {
 			System.out.println("invalid argument!");
 		} else {
 			try {
-				final Server server = new Server(args[0]);
-				Naming.rebind(server.getServerUrl(), server);
+				Boolean alreadyRunningBoolean = false;
+
+				final String[] list = Naming.list("rmi://" + HOST + "/Server/");
+				for (final String string : list) {
+					final ServerInterface srvInterface = (ServerInterface) Naming.lookup(string);
+					if (srvInterface.getServerNameString().equals(args[0].toString())) {
+						alreadyRunningBoolean = true;
+					}
+				}
+				if (alreadyRunningBoolean == false) {
+					final Server server = new Server(args[0]);
+					Naming.rebind(server.getServerUrl(), server);
+				} else {
+					System.out.println("Namesake server running!");
+				}
 			} catch (final Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
+
+	private static final String HOST = "localhost";
 
 }
