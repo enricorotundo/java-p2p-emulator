6ecdb0bdc73c5c06aef7e1ff5840969fb75baeca
tundo91 <enrico.rotundo@gmail.com>
2014-09-07 18:05:49 +0200

    previous commit non serviva a nulla

--- src/view/ServerFrame.java
+++ src/view/ServerFrame.java
@@ -77,19 +77,14 @@ public class ServerFrame extends AbstractBasicFrame implements Observer {
 	// chiamato da model.server.ConnectedClients  al quale chiede i dati da visualizzare
 	public final void updateConnectedClients() {	
 		// chiamare model.ConnectedClients.getConnectedClientsModel();		
-		final DefaultListModel modelConnectedClients = connectedClients.getConnectedClientsDefaultListModel();
-		if (modelConnectedClients.size() > 0) {
-			connectedServersList.setModel(modelConnectedClients);			
-		}
+		connectedServersList.setModel(connectedClients.getConnectedClientsDefaultListModel());			
 	}
 	
 	// chiamato da model.server.ConnectedServers al quale chiede i dati da visualizzare
 	public final void updateConnectedServers() {
 		// chiamare model.ConnectedServers.getConnectedServersModel();
-		final DefaultListModel modelConnectedServers = connectedServers.getConnectedServersModel();
-		if (modelConnectedServers.size() > 0) {
-			connectedServersList.setModel(modelConnectedServers);			
-		}
+		connectedServersList.setModel(connectedClients.getConnectedClientsDefaultListModel());			
+		
 	}
 
 	// invocato quando il MODEL viene modificato -> aggiorna la VIEW
