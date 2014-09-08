572726937cbc43a1376d1a23e31f8e5214aba4df
tundo91 <enrico.rotundo@gmail.com>
2014-09-09 00:52:52 +0200

    dsf

--- src/controller/client/ConnectionChecker.java
+++ src/controller/client/ConnectionChecker.java
@@ -29,7 +29,7 @@ class ConnectionChecker extends Thread {
 			try {
 				sleep(100);
 				synchronized (connectionStatusUp) {
-					
+					System.out.println("ConnectionChecker running");
 					// controllo lo stato della connessione solo se ero/sono connesso
 					if (connectionStatusUp.get() == true) {
 						ServerInterface remoteServerInterface = (ServerInterface) Naming.lookup(Server.URL_STRING + serverName);	
@@ -39,12 +39,12 @@ class ConnectionChecker extends Thread {
 			} catch (final NotBoundException | MalformedURLException | RemoteException | InterruptedException e) {
 				
 				// qui il server a cui ero connesso non esiste piu'
+				
+				//setto la connessione come down
 				synchronized (connectionStatusUp) {
 					connectionStatusUp.set(false);
 				}
-					
 
-				
 				synchronized (this) {
 					try {
 						gui.setConnectionButtonText("Connect");						
