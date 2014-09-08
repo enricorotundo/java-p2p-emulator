631569cf934637ac4a8457666739464e7b3878da
tundo91 <enrico.rotundo@gmail.com>
2014-09-08 10:34:55 +0200

    fix srv gui

--- src/view/ServerFrame.java
+++ src/view/ServerFrame.java
@@ -85,8 +85,8 @@ public class ServerFrame extends AbstractBasicFrame implements Observer {
 	// chiamato da model.server.ConnectedServers al quale chiede i dati da visualizzare
 	public final void updateConnectedServers() {
 		// chiamare model.ConnectedServers.getConnectedServersModel();
-		if (connectedClients.getConnectedClientsDefaultListModel().size() > 0) {
-			connectedServersList.setModel(connectedClients.getConnectedClientsDefaultListModel());						
+		if (connectedServers.getConnectedServersModel().size() > 0) {
+			connectedServersList.setModel(connectedServers.getConnectedServersModel());						
 		}
 	}
 
